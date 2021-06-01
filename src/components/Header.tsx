1e3b4c651454a9f73b0f2c1edf6aaca58c854db8
Jalal Mallah <jalal_mallah@live.com>
2021-06-01 09:06:42 +0200

    style: style the Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
+import styles from 'styles/Header.module.scss';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Header: React.FC = () => {
   return (
-    <header>
+    <header className={styles.header}>
       <h1>Contacts</h1>
       <form>
         <label htmlFor='user-search'>
           <FontAwesomeIcon icon={faSearch} />
         </label>
-        <input type='text' id='user-search' />
+        <input type='text' id='user-search' placeholder='Search term...' />
       </form>
     </header>
   );
