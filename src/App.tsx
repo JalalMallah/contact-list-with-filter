0b4dad0df798b6d5d826cca9e0a4e794ce7dc686
Jalal Mallah <jalal_mallah@live.com>
2021-06-01 09:10:32 +0200

    feat: create ContactList component and import it to App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from 'components/Header';
+import ContactList from 'components/ContactList';
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
+      <ContactList />
     </>
   );
 };
