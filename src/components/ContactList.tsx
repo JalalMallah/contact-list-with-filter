f1a0170e0fd131169d2e0d9f3076f655c9bc6a98
Jalal Mallah <jalal_mallah@live.com>
2021-06-01 09:17:49 +0200

    feat: create ContactItem component and import it to ContactList component

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
+import ContactItem from 'components/ContactItem';
+
 import styles from 'styles/ContactList.module.scss';
 
 const ContactList: React.FC = () => {
   return (
     <ul className={styles.contactList}>
-      <li>item 1</li>
-      <li>item 2</li>
-      <li>item 3</li>
-      <li>item 4</li>
-      <li>item 5</li>
+      <ContactItem />
+      <ContactItem />
+      <ContactItem />
     </ul>
   );
 };
