8c4153204261b0f133be25675d251ba10c80dd72
Jalal Mallah <jalal_mallah@live.com>
2021-06-01 11:14:11 +0200

    style: style the contact items accordingly to the mockup

--- src/components/ContactItem.tsx
+++ src/components/ContactItem.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Contact } from 'interfaces/ContactInterface';
 import styles from 'styles/ContactItem.module.scss';
 
-const ContactItem: React.FC<Contact> = ({ first_name, last_name }) => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle, faCheckCircle } from '@fortawesome/free-regular-svg-icons';
+
+const ContactItem: React.FC<Contact> = ({ avatar, email, first_name, id, last_name }) => {
+  const [isContactChecked, setIsContactChecked] = useState<boolean>(false);
+
+  const avatarElement = avatar ? (
+    <img src={avatar} alt="contact's avatar" />
+  ) : (
+    <span>{first_name.charAt(0).toUpperCase() + last_name.charAt(0).toUpperCase()}</span>
+  );
+
+  const checkCircleElement = isContactChecked ? (
+    <FontAwesomeIcon icon={faCheckCircle} />
+  ) : (
+    <FontAwesomeIcon icon={faCircle} />
+  );
+
+  function handleContactItemClick() {
+    setIsContactChecked(prev => !prev);
+  }
+
   return (
-    <li className={styles.contactItem}>
-      {first_name} {last_name}
+    <li className={styles.contactItem} onClick={handleContactItemClick}>
+      <div className={styles.avatarContainer}>{avatarElement}</div>
+      <div className={styles.contactInfo}>
+        <p className={styles.name}>
+          {first_name} {last_name}
+        </p>
+        <p className={styles.email}>{email}</p>
+      </div>
+      <div className={styles.checkCircle}>{checkCircleElement}</div>
     </li>
   );
 };
